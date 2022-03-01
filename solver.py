6a915230b7a446497b28ffd718e8719f7219d5d8
bogey3 <29926385+bogey3@users.noreply.github.com>
2022-02-28 19:24:18 -0500

    Update solver.py

--- solver.py
+++ solver.py
@@ -1,3 +1,4 @@
+import random
 import time
 import io
 from PIL import Image
@@ -50,8 +51,9 @@ def enterWord(browser, word):
 def checkWebAnswer(guess, screenshot, row):
     letters = {}
     image = Image.open(io.BytesIO(screenshot))
+
     for column in range(5):
-        r, g, b, a = image.getpixel((117 + (column*84), 118 + (row * 85)))
+        r, g, b, a = image.getpixel((115 + (column*84), 115 + (row * 84)))
 
         if r == 83 and b == 78 and g == 141:
             letters[guess[column]] = [column]
@@ -64,24 +66,44 @@ def checkWebAnswer(guess, screenshot, row):
 
     return letters
 
+def countFoundLetters(letters):
+    found = 0
+    for letter, locations in letters.items():
+        if locations != None:
+            found += 1
+    return found
+
 if __name__ == '__main__':
     browser = openWordle()
     words = []
     validWords = []
+    letters = {}
     with open("5-letter-words.txt", "r") as f:
         words = f.readlines()
     words = list(map(str.strip, words))
+    validWords = words
 
-    for word in words:
-        if len(word) == 5:
-            validWords.append(word)
 
-    for guesses, word in enumerate(["bring", "claps", "sweat", "joked", "tramp"]):
+    for guesses, word in enumerate(["train", "chose", "drums", "felty", "kapow"]):
+        if countFoundLetters(letters) == 5:
+            word = random.choice(validWords)
         result = enterWord(browser, word)
-        checkWebAnswer(word, result, guesses)
-        for letter, value in checkWebAnswer(word, result, guesses).items():
-            validWords = removeWords(validWords, letter, value)
+        newLetters = checkWebAnswer(word, result, guesses)
+        for letter, value in newLetters.items():
+            if value == None or len(value) == 1 or letter not in letters:
+                letters[letter] = value
+            elif value != letters[letter]:
+                    oldLocations = letters[letter]
+                    letters[letter] = []
+                    for location in value:
+                        if location in oldLocations:
+                            letters[letter].append(location)
+
+        for letter in newLetters.keys():
+            validWords = removeWords(validWords, letter, letters[letter])
 
         if len(validWords) == 1:
             enterWord(browser, validWords[0])
             break
+    if len(validWords) > 1:
+        browser.execute_script("alert('I was not able to find the word, here are the possible words:\\n" + "\\n".join(validWords) + "')")
